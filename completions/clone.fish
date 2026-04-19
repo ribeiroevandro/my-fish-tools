66d21bae4b62282628dbb831ac144d1967bf9b63
Evandro Ribeiro <ribeiroevandro@live.com>
2026-04-19 04:44:58 -0300

    fix(clone): add URL protocol hints to completions
    Show git@ and https:// as completion hints when no URL has been
    provided yet, making it clear a repository URL is expected as the
    first argument.

--- completions/clone.fish
+++ completions/clone.fish
@@ -10,6 +10,8 @@ function __clone_complete_editors
 end
 
 complete -c clone -n 'not __fish_is_nth_token 1' -a "(__clone_complete_editors)" -d "Editor"
-# Show flags as hints when no URL provided yet
+# URL hints for position 1
+complete -c clone -n '__fish_is_nth_token 1' -a "git@" -d "Repository URL"
+complete -c clone -n '__fish_is_nth_token 1' -a "https://" -d "Repository URL"
 complete -c clone -n '__fish_is_nth_token 1' -a "--help" -d "Show usage"
 complete -c clone -n '__fish_is_nth_token 1' -a "--enter" -d "Enter directory after cloning"
